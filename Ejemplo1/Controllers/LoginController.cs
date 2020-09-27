feb83c8744e4109619817a3f000a51c2b9b7ced3
Mrjarkos <javiercardenas@usantotomas.edu.co>
2020-09-26 21:38:46 -0500

    Conexión con base de datos en la nube

--- Ejemplo1/Controllers/LoginController.cs
+++ Ejemplo1/Controllers/LoginController.cs
@@ -66,7 +66,7 @@ namespace Ejemplo1.Controllers
                     ViewBag.error = true;
                     ViewBag.mensaje = "Error:" + ex.Message.ToString();
                 }
-                return View();
+                return View("Login");
         }
 
         [HttpPost]
